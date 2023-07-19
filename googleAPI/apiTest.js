9e81fb6fb4b97651fea3567900c9857a95a9ea4d
Seonghyun Kim <archun39@kookmin.ac.kr>
2023-07-18 22:17:56 -0700

    get cafe name and reviews from place_id

--- googleAPI/apiTest.js
+++ googleAPI/apiTest.js
@@ -17,14 +17,27 @@ async function getCafePlaceIds(){
                 },
             }
         );
-        console.log(apiKey);
+
         const placeIds = response.data.results.map((result) => result.place_id);
 
-        for(const placesId of placeIds){
+
+        console.log('Cafe Name : ' , (await getCafeInfo(placeIds[0])).name);
+        console.log('Reviews Num : ',(await getCafeInfo(placeIds[0])).reviews.length);
+        for(const review of (await getCafeInfo(placeIds[0])).reviews){
+
+            console.log(review.text,`\n`);
+
+        }
+
+
+
+        /*for(const placesId of placeIds){
+
             console.log('Cafe place_id : ' ,placesId);
             const cafeInfo = await getCafeInfo(placesId);
-            console.log('Cafe Name : ', cafeInfo);
-        }
+            console.log('Cafe Name : ', cafeInfo.name);
+            console.log('Cafe Address : ', cafeInfo.address);
+        }*/
 
     }
     catch (error){
@@ -40,10 +53,18 @@ async function getCafeInfo(placeId){
                 params: {
                     key: apiKey,
                     place_id: placeId,
-                    fields: 'name',
+                    fields: `name,formatted_address,reviews`, //가게 이름만 가져오게 호출함
                 }
             }
         );
+        const placeTempInfo = response.data.result;
+
+        return {
+            name : placeTempInfo.name,
+            address : placeTempInfo.formatted_address,
+            reviews : placeTempInfo.reviews,
+
+        }
     }
     catch (error){
         console.error('Error fetching cafe info from Google Places API: ',error);
