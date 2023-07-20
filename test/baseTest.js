7f67bfab5d3a5fe02814ecd20278be3bb282a792
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-20 16:58:44 -0700

    automation complete

--- test/baseTest.js
+++ test/baseTest.js
@@ -1,17 +1,27 @@
-import chai, { expect } from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
 import * as fs from "fs";
 import Sequelize from 'sequelize';
 
 chai.use(chaiHttp);
 
-const sql_string = fs.readFileSync("/db/app_migration.sql", 'utf-8');
-const sequelize = new Sequelize('database', 'username', 'password', {
+const drop_tables_sql_string = fs.readFileSync("test/db/drop_tables.sql", 'utf-8');
+const create_tables_sql_string = fs.readFileSync("test/db/app_migration.sql", 'utf-8');
+
+const sequelize = new Sequelize('franfe', 'root', '1234', {
   host: 'localhost',
-  port: 33060,
+  port: 33070,
   dialect: 'mysql',
 })
 
-export function resetTestDb() {
-  sequelize.query(sql_string);
+export async function resetTestDb() {
+  try{
+    await sequelize.query(drop_tables_sql_string);
+    await sequelize.query(create_tables_sql_string);
+  } catch (error) {
+    console.error(error);
+  }  finally {
+    sequelize.close();
+  }
+
 }
