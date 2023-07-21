081698fe0604c17fb898f73ceb26445f2d0ad55e
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-21 09:52:05 -0700

    swagger

--- src/routes/googleMapRoutes.js
+++ src/routes/googleMapRoutes.js
@@ -4,7 +4,20 @@ import { bcd } from '../domain/googleMap/googleMapTransactionService';
 let router;
 export default router = Router();
 
-router.get('/googleMap', (req) => {
+router.get('/googleMap/:id', (req) => {
+  const requestUuid = req.query.uuid;
+  const response = bcd(req);
+  response.json(response);
+});
+
+router.post('/googleMap/:id', (req) => {
+  const requestUuid = req.query.uuid;
+  /* #swagger.parameters['obj'] = {
+          in: 'body',
+          description: 'Informações do usuário',
+          schema: { $ref: "#/definitions/AddUser" }
+   } */
+  const request = req.body;
   const response = bcd(req);
   response.json(response);
 });
---
+++ swagger.js
@@ -0,0 +1,30 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const outputFile = './swagger_output.json'
+const endpointsFiles = ['./src/routes/googleMapRoutes.js']
+
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Franfe Backend",
+    description: "Franfe Backend <b>Api</b>."
+  },
+  host: "localhost:3000",
+  basePath: "/",
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  contact: {
+    name: "Rogan Oh",
+    email: "fdscbjdcnhd@gmail.com",
+  },
+  tags: [
+    {
+      "name": "User",
+      "description": "Endpoints"
+    }
+  ],
+}
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
