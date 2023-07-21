53c3732908b7ca32b28380c840ba621c58ab2615
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-21 11:04:51 -0700

    fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function (err, req, res, next) {
   res.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error});
 });
 
 export default app;
