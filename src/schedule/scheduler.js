4acc07eccdd4ec8f9b9a89d3f65236e649945c19
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-22 11:50:02 -0700

    creating job successfully

--- src/schedule/scheduler.js
+++ src/schedule/scheduler.js
@@ -1,9 +1,5 @@
 import nodeSchedule from 'node-schedule';
 
-// const job = nodeSchedule.scheduleJob('* * * * *', () => {
-//   console.log('Running scheduler');
-// })
-
 export function jobGenerator(cron) {
   return nodeSchedule.scheduleJob(cron, () => {
     console.log('Running scheduler');
