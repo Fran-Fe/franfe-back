dd08e36520cdd07e1a0d163e4cbd19867a5b76d6
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-23 18:48:57 -0700

    migration script

--- test/db/app_migration.sql
+++ test/db/app_migration.sql
@@ -17,3 +17,39 @@ CREATE TABLE if not EXISTS `cafe_click_counts` (
   PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `cafe_options` (
+  `cafe_uuid` varchar(45) NOT NULL,
+  `option` varchar(45) NOT NULL,
+  `option_on` tinyint(1) NOT NULL DEFAULT '0'
+);
+
+CREATE TABLE `cafe_hashtags` (
+  `cafe_uuid` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `hashtag` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+);
+
+CREATE TABLE `cafe_reviews` (
+  `id` bigint NOT NULL,
+  `cafe_uuid` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `author_name` varchar(45) NOT NULL,
+  `rating` decimal(2,1) NOT NULL,
+  `relative_time_description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `cafe_thumbnail_s3` (
+  `bucket_url` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `cafe_uuid` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `category` int NOT NULL,
+  PRIMARY KEY (`bucket_url`,`cafe_uuid`),
+  UNIQUE KEY `bucket_url` (`bucket_url`)
+);
+
+CREATE TABLE `cafe_review_texts` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cafe_review_id` bigint NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
