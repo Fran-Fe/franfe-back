4465dbe0ff28c3af614dfd1a129960567ad11b5b
Seonghyun Kim <archun39@kookmin.ac.kr>
2023-07-25 13:06:05 -0700

    merge

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import router from './src/routes/googleMapRoutes.js';
 import { router as cafeRouter } from './src/routes/forAI/cafesRoutes.js'
 import { router as cafeRankingRouter } from './src/routes/forFront/cafeRankingRoutes.js'
 import { router as cafeInfoRouter } from './src/routes/forFront/cafeInfoRoutes.js'
+import { router as cafeLocationRouter } from './src/routes/forFront/getCafeLocationsRoutes.js'
 import { jobGenerator } from "./src/schedule/scheduler.js";
 
 const app = express();
