c0178f7b5a8ad6a37c2fd0d2086bc884625acc2f
roganOhDev <fdscbjdcnhd@gmail.com>
2023-08-02 11:56:51 -0700

    id for tables

--- migrations/real_queries/2023-07-22-20-38-00-v_1_1.sql
+++ migrations/real_queries/2023-07-22-20-38-00-v_1_1.sql
@@ -1,4 +1,5 @@
 CREATE TABLE if not EXISTS `cafes` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `uuid` varchar(150) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `address` varchar(100) NOT NULL,
   `lat` decimal(10,7) NOT NULL DEFAULT '123.1234567',
@@ -6,5 +7,5 @@ CREATE TABLE if not EXISTS `cafes` (
   `place_name` varchar(45) NOT NULL,
   `overview` varchar(150) DEFAULT NULL,
   `rating` decimal(2,1) NOT NULL DEFAULT '5.0',
-  PRIMARY KEY (`uuid`)
+  PRIMARY KEY (`id`)
 );
