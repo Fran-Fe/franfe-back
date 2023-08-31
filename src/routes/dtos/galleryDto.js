27d92ec67ff9e37f4e48e3a5bfcb1493c5f04375
roganOhDev <donggeun.oh@orionic.com>
2023-08-31 14:25:46 -0700

    fix for gallery category filter

--- src/routes/dtos/galleryDto.js
+++ src/routes/dtos/galleryDto.js
@@ -1,7 +1,7 @@
 export class galleryDto{
   static Request = class{
     constructor(req, reqCategory) {
-      this.pageNum = Number(req.query.pageNum);
+      this.pageNumber = Number(req.query.pageNumber);
       this.pageSize = Number(req.query.pageSize);
       this.category = Number(reqCategory);
     }
